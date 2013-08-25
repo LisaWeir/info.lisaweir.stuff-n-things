f5375a1a2a4e4afa90516eeb2617ec0db4d27c88
Lisa Dietrich <lisa@Lisa-Dietrichs-MacBook-Air.local>
2013-08-24 23:29:05 -0700

    Initial Commit

---
+++ Stuff and Things/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stuff and Things
+//
+//  Created by Lisa Dietrich on 8/24/13.
+//  Copyright (c) 2013 lisadietrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stuff and Things/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stuff and Things
+//
+//  Created by Lisa Dietrich on 8/24/13.
+//  Copyright (c) 2013 lisadietrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
