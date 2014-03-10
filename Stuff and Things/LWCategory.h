67d1efd4f26b5bcd24379917a14d9518925ae855
Lisa Weir <lisa@Lisas-iMac.local>
2014-03-10 00:27:34 -0700

    Started work on model

---
+++ Stuff and Things/LWCategory.h
@@ -0,0 +1,15 @@
+//
+//  LWCategory.h
+//  Stuff and Things
+//
+//  Created by Lisa Weir on 3/10/14.
+//  Copyright (c) 2014 lisadietrich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LWCategory : NSObject
+
+@property (strong, nonatomic) NSMutableArray *activities;
+
+@end
---
+++ Stuff and Things/LWCategory.m
@@ -0,0 +1,14 @@
+//
+//  LWCategory.m
+//  Stuff and Things
+//
+//  Created by Lisa Weir on 3/10/14.
+//  Copyright (c) 2014 lisadietrich. All rights reserved.
+//
+
+#import "LWCategory.h"
+
+@implementation LWCategory
+
+
+@end
